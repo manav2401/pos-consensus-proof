dd6e3322421a40e109ae10bdee20ed104fdc25c4
Manav Darji <manavdarji.india@gmail.com>
2024-12-27 00:26:29 +0700

    (feat): optimise getEncodedValidatorInfo contract call (#31)
    This PR optimises the contract call used to fetch active validator set info.

--- contracts/src/ConsensusProofVerifier.sol
+++ contracts/src/ConsensusProofVerifier.sol
@@ -7,7 +7,9 @@ import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 interface StakeManager {
     // Borrowed from the StakeManager contracts
     enum Status {Inactive, Active, Locked, Unstaked}
-    function NFTCounter() external view returns (uint256);
+    function signers(uint256) external view returns (address);
+    function signerToValidator(address) external view returns (uint256);
+    function currentValidatorSetSize() external view returns (uint256);
     function validators(uint256) external view returns (uint256, uint256, uint256, uint256, uint256, address, address, Status, uint256, uint256, uint256, uint256, uint256);
     function validatorState() external view returns (uint256, uint256);
 }
@@ -41,47 +43,21 @@ contract ConsensusProofVerifier {
 
     /// @notice Fetches the active validator info like signer address, respective stake, and 
     ///         total stake and returns the encoded data.
-    /// @return encodedValidatorInfo abi encoded data of all active validators
-    function getEncodedValidatorInfo() public view returns(address[] memory, uint256[] memory, uint256) {
-        // Get the total number of validators stored by fetching the NFT count. The count is
-        // assigned to the next validator and hence we subtract 1 from it.
-        uint256 length = StakeManager(posStakeManager).NFTCounter() - 1;
-
-        address[] memory signers = new address[](length);
-        uint256[] memory stakes = new uint256[](length);
-        bool[] memory isActive = new bool[](length);
-        uint256 totalActive = 0;
-
-        // Validator index starts from 1.
-        for (uint256 i = 1; i <= length; i++) {
-            uint256 selfStake;
-            uint256 delegatedStake;
-            address signer;
-            StakeManager.Status status;
-            (selfStake, , , , , signer, , status, , , ,delegatedStake,) = StakeManager(posStakeManager).validators(i);
-            signers[i-1] = signer;
-            stakes[i-1] = selfStake + delegatedStake;
-            isActive[i-1] = status == StakeManager.Status.Active;
-            if (isActive[i-1]) {
-                totalActive += 1;
-            }
+    /// @return activeValidators list of active validators in set
+    /// @return stakes list of respective stake of active validators in set
+    /// @return totalStake total stake of whole validator set
+    function getEncodedValidatorInfo() public view returns (address[] memory, uint256[] memory, uint256) {
+        uint256 totalValidators = StakeManager(posStakeManager).currentValidatorSetSize();
+        address[] memory activeValidators = new address[](totalValidators);
+        uint256[] memory stakes = new uint256[](totalValidators);
+        for (uint256 i = 0; i < totalValidators; i++) {
+            activeValidators[i] = StakeManager(posStakeManager).signers(i);
+            (uint256 selfStake, , , , , , , , , , , uint256 delegatedStake, ) = StakeManager(posStakeManager).validators(StakeManager(posStakeManager).signerToValidator(activeValidators[i]));
+            stakes[i] = selfStake + delegatedStake;
         }
-
-        address[] memory activeSigners = new address[](totalActive);
-        uint256[] memory activeStakes = new uint256[](totalActive);
-
-        uint256 j = 0;
-        for (uint256 i = 0; i < length; i++) {
-            if (isActive[i]) {
-                activeSigners[j] = signers[i];
-                activeStakes[j] = stakes[i] / 1e18;
-                j++;
-            }
-        }
-
         uint256 totalStake;
         (totalStake, ) = StakeManager(posStakeManager).validatorState();
-        return (activeSigners, activeStakes, totalStake / 1e18);
+        return (activeValidators, stakes, totalStake / 1e18);
     }
 
     /// @notice The entrypoint for the verifier.
