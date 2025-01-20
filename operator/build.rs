6eacee7d01e2abbbde8b8ee2d4c6dc2f4495162c
Manav Darji <manavdarji.india@gmail.com>
2025-01-20 18:14:08 +0530

    handle 3hr time window in operator, update elf, minor fixes

--- operator/build.rs
+++ operator/build.rs
@@ -3,6 +3,7 @@ use sp1_helper::{build_program_with_args, BuildArgs};
 fn main() {
     let args = BuildArgs {
         ignore_rust_version: true,
+        output_directory: Some("../elf".to_string()),
         elf_name: Some("pos-consensus-proof".to_string()),
         ..Default::default()
     };
